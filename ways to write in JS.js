b77ddd8a266db05bdb2ca717523dc3472aa71038
vishalpatil0 <vishalgpatil10@gmail.com>
2021-06-16 12:35:27 +0530

    basic JS codes

---
+++ ways to write in JS.js
@@ -0,0 +1,7 @@
+// Ways to print in javascript
+console.log("hello world");
+alert("hello world");
+document.write("this is document write ");
+console.error("this is an erro");
+console.warn("this is an warning");
+console.log("wayus to print in javascript");
